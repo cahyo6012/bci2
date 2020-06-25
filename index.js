f8c571c1ffd61894b4a4427d11247e60e3d30096
cahyo adi <dev.cahyo6012@gmail.com>
2020-06-26 01:45:40 +0700

    Initial Commit

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const BCI = require('./lib/BCI')
 const { bci_config } = require('./lib/config')
 
-(async function() {
+async function main() {
   const bci = new BCI()
   await bci.login(bci_config.username, bci_config.password)
   await bci.downloadAllProjects()
   await bci.importProjects()
-
   await bci.logout()
-})()
+}
 
-console.log(bci_config)
+main()
